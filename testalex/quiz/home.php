2931957c892a88b62bca6062a00976446e418a76
Alexandre Do <a.do@ecole-ipssi.net>
2023-03-09 10:26:02 +0100

    purple

--- testalex/quiz/home.php
+++ testalex/quiz/home.php
@@ -40,9 +40,11 @@ if(isset($_GET['role'])) {
         });
       </script>
     <header>
-        <a class="home" href="home.html">
-            <span>Quiz</span><span>zeo.</span>
-        </a>
+        <?php
+            echo "<a class='home' href='home.php?role=$role&user=$id_user'>";
+            echo "<span>Quiz</span><span>zeo.</span>";
+            echo "</a>";
+        ?>
         <div class="options">
             <?php 
                 $sqlutilisateur = "SELECT * FROM utilisateur WHERE Id_utilisateur = '$id_user'";
