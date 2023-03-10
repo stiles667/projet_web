ca1b4381337a62a1d3c47e9a0e30e0ead24d9e14
Alexandre Do <a.do@ecole-ipssi.net>
2023-03-10 20:01:51 +0100

    LE QUIZZ FONCTIONNE

--- quiz.php
+++ quiz.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 
 require ('bdconnexion.php');
 
@@ -17,7 +18,56 @@ if(isset($_GET['role'])) {
     $role = $_GET['role'];
 }
 
+
+$questions = array();
+                    
+if (mysqli_num_rows($result) > 0) {
+    while($row1 = mysqli_fetch_assoc($result)) {
+        $id_question = $row1['Id_question'];
+        $question = $row1['question'];
+                            
+        $sql2 = "SELECT * FROM choix WHERE Id_question = " . $id_question;
+        $result2 = mysqli_query($conn, $sql2);
+        $reponsesjs = array();
+        if (mysqli_num_rows($result2) > 0) {
+            while($row2=mysqli_fetch_assoc($result2)) {
+                // $idquest = $row2['Id_question'];
+
+                $reponses = array(
+                    // 'idquestion ' => $row2['Id_question'],
+                    'reponse1' => $row2['rep1'],
+                    'reponse2' => $row2['rep2'],
+                    'reponse3' => $row2['rep3'],
+                    'Bonne_rep'=> $row2['Bonne_reponse']
+                );
+
+                $rep1 = $row2['rep1'];
+                $rep2 = $row2['rep2'];
+                $rep3 = $row2['rep3'];
+                $bonnerep = $row2['Bonne_reponse'];
+
+                $reponsesjs[] = $reponses;
+            }
+        }
+                            
+        $questions[] = array(
+            // 'id' => $id_question,
+            'question' => $question
+            // 'reponses' => $reponsesjs
+        );
+
+        $rep[] = array(
+            // 'idquestion' => $id_question,
+            'reponse1' => $rep1,
+            'reponse2' => $rep2,
+            'reponse3' => $rep3,
+            'Bonne_rep' => $bonnerep
+        );
+    }
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -40,7 +90,6 @@ if(isset($_GET['role'])) {
         Your browser does not support the audio element.
       </audio>
       
-
       <script>
         document.addEventListener('DOMContentLoaded', () => {
           const audio = document.querySelector('#audio');
@@ -48,273 +97,179 @@ if(isset($_GET['role'])) {
         });
       </script>
     <header>
-        <?php
-            echo "<a class='home' href='home.php?role=$role&user=$id_user'>";
-            echo "<span>Quiz</span><span>zeo.</span>";
-            echo "</a>";
-        ?>
+        <a class="home" href="accueil.html">
+            <span>Quiz</span><span>zeo.</span>
+        </a>
         <div class="options">
-        <?php 
-                $sqlutilisateur = "SELECT * FROM utilisateur WHERE Id_utilisateur = '$id_user'";
-                $resultutilisateur = mysqli_query($conn, $sqlutilisateur);
-
-                $row = mysqli_fetch_assoc($resultutilisateur);
-                $role_user = $row['role_utilisateur'];
-                $pseudo = $row['pseudo'];
-                $email = $row['email'];
-                $id_utilisateur = $row['Id_utilisateur'];
-
-                echo "<h2>$pseudo</h2>";
-                echo "<a id='profil' href='dashboard.php?role=$role&user=$id_user'>";
-                echo "<img src='https://cdn-icons-png.flaticon.com/512/149/149071.png' alt='Photo de profil'>";
-                echo "</a>";
-            ?>
-        </div>
+            <h2>Utilisateur</h2>
+            <a id="profil" href="dashboard.html">
+                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="Photo de profil">
+            </a>
         </div>
         </div>
     </header>
     <div class="container">
-        <div class="container-question">
+<body>
+<div class="container-question">
             <div class="number-question">
-                <h2>
-                    <?php 
-                    $numquestion = 1;
-                    echo "Question " . $numquestion . "/10";
-                    $numquestion++;
-                    if ($numquestion > 10) {
-                        $numquestion = 1;
-                    }
-                    ?>
-                </h2>
-                <div class="container-countdown">
+                <h2 id="number-question"></h2>
+            
+                <div class="container-countdown" id="container-countdown">
                     <h2 id="countdown">60</h2>
                 </div>
+
+                <div class="score">
+                    <h2 id="score">Votre score : 1/10</h2>
+                </div>
             </div>
         </div>
-        <div class="question">
-                <h3></h3>
-            </div>
         <div class="container-answer">
-            
-            <form action="#">
-                <div class="answers">
-
-                    <label class="answer">
-                        <input type="radio" name="option" value="option1">
-                        Rouge
-                    </label>
-
-                    <label class="answer">
-                        <input type="radio" name="option" value="option2">
-                        Blanc
-                    </label>
-
-                    <label class="answer">
-                        <input type="radio" name="option" value="option3">
-                        Noir
-                    </label>
-
-                    <label class="answer">
-                        <input type="radio" name="option" value="option3">
-                        Vert
-                    </label>
-
-                    <?php
-                    $questions = array();
+            <div class="question" id="question">
                     
-                    if (mysqli_num_rows($result) > 0) {
-                        while($row1 = mysqli_fetch_assoc($result)) {
-                            $id_question = $row1['Id_question'];
-                            $question = $row1['question'];
-                            
-                            $sql2 = "SELECT * FROM choix WHERE Id_question = " . $id_question;
-                            $result2 = mysqli_query($conn, $sql2);
-                            $reponsesjs = array();
-                            if (mysqli_num_rows($result2) > 0) {
-                                while($row2=mysqli_fetch_assoc($result2)) {
-                                    // $idquest = $row2['Id_question'];
-
-                                    $reponses = array(
-                                        // 'idquestion ' => $row2['Id_question'],
-                                        'reponse1' => $row2['rep1'],
-                                        'reponse2' => $row2['rep2'],
-                                        'reponse3' => $row2['rep3'],
-                                        'Bonne_rep'=> $row2['Bonne_reponse']
-                                    );
-
-                                    $rep1 = $row2['rep1'];
-                                    $rep2 = $row2['rep2'];
-                                    $rep3 = $row2['rep3'];
-                                    $bonnerep = $row2['Bonne_reponse'];
-
-                                    $reponsesjs[] = $reponses;
-                                }
-                            }
-                            
-                            $questions[] = array(
-                                // 'id' => $id_question,
-                                'question' => $question
-                                // 'reponses' => $reponsesjs
-                            );
-
-                            $rep[] = array(
-                                // 'idquestion' => $id_question,
-                                'reponse1' => $rep1,
-                                'reponse2' => $rep2,
-                                'reponse3' => $rep3,
-                                'Bonne_rep' => $bonnerep
-                            );
-                        }
-                    }
-                    
-                    // print_r($questions);
-                    // print_r($questions);
-                    // print_r($rep);
-                    // $num_question = 1;
-                    // $questions = array ();
-                    // while($row=mysqli_fetch_assoc($result)) {
-                    //     $id_question = $row['Id_question'];
-                    //     $question = $row['question'];
-                        
-                        
-                        
-                        
-                        
-                        
-
-                        // foreach ($questions as $index => $question) {
-                        //     echo $question['question'];
-                        // }
-
-                        //affiche la premiere question
-                        // echo "" .$num_question .". " .$question;
-                        // $num_question++;
-                        // if ($numquestion > 10) {
-                        //     $numquestion = 1;
-                        // }
-                        
-
-                        // $sql2 = "SELECT * FROM choix WHERE Id_question = " . $id_question;
-                        // $result2 = mysqli_query($conn, $sql2);
-                        // $reponsesjs = array();
-                        // if (mysqli_num_rows($result2) > 0) {
-                        //     while($row2=mysqli_fetch_assoc($result2)) {
-                        //         $reponses = array(
-                        //             // 'idquestion ' => $row2['Id_question'],
-                        //             'reponse1' => $row2['rep1'],
-                        //             'reponse2' => $row2['rep2'],
-                        //             'reponse3' => $row2['rep3'],
-                        //             'Bonne_rep'=> $row2['Bonne_reponse']
-                        //         );
-
-                        //         foreach ($reponses as $index => $reponse) {
-                        //             $reponsesjs[] = $reponses;
-                        //         }
-
-                                
-                                
-                                
-
-                        //         shuffle($reponses);
-
-
-                                // foreach ($reponses as $index => $reponse) {
-                            
-                                //     echo '<label class="answer">
-                                //     <input type="radio" name="option" value="'.$index.'">
-                                //     '.$reponse.'
-                                //     </label>';
-                                // }
-                                
-                    //         } 
-                    //     }
-
-                        
-                    // }
-                    
-                        // echo "<ol>";
-                        // for ($i = 0; $i < 10 && $i < count($questions); $i++) {
-                        //     echo "<li>" . $questions[$i]['question'] . "</li>";
-                        // }
-                        // echo "</ol>";
-                        // echo "<ol>";
-
-
-                        // echo json_encode($questions);
-                        // print_r($questions);
-                        // echo "<br>";
-                        // print_r($reponsesjs);
-
-
-                    ?>
-                </h3>
-            </div>
-            
-            <!-- <form action="#">
-                <div class="answers">
-                    
-                    // $sql2 = "SELECT * FROM choix WHERE Id_question = " . $id_question;
-                    // $result2 = mysqli_query($conn, $sql2);
-                    // if (mysqli_num_rows($result2) > 0) {
-                    //     while($row2=mysqli_fetch_assoc($result2)) {
-                    //         $reponses = array(
-                    //             'reponse1' => $row2['rep1'],
-                    //             'reponse2' => $row2['rep2'],
-                    //             'reponse3' => $row2['rep3'],
-                    //             'Bonne_rep'=> $row2['Bonne_reponse']
-                    //         );
-
-                    //         shuffle($reponses);
-
-                    //         foreach ($reponses as $index => $reponse) {
-                        
-                    //             echo '<label class="answer">
-                    //             <input type="radio" name="option" value="'.$index.'">
-                    //             '.$reponse.'
-                    //             </label>';
-                    //         }
-                            
-                    //     } 
-                    // }
-                    ?>
                 </div>
-                <button type="submit" id="submit">Valider</button>
+                <div class="answers" id=answers>
+
+                    
+                    </div>
+                <button type="submit" id="valider">Valider</button>
             </form>
         </div>
-    </div> -->
-    <script>
-        const _quest = document.getElementById('question');
-        const _answers = document.querySelector('.answers');
-        var questions = <?php echo json_encode($questions) ?>;
-        // console.log(questions);
-        
-        var reponses = <?php echo json_encode($rep) ?>;
-        // console.log(reponses);
+    </div>
 
-        var numquestion = 1;
-        var quizzarray = 0;
-        
-        var quizz = [questions, reponses];
-        console.log(quizz);
+	<script>
+		const _quest = document.getElementById('question');
+		const _answers = document.querySelector('.answers');
+		const _submit = document.getElementById('valider');
+        const _nbquestion = document.getElementById('number-question');
+        const _score = document.getElementById('score');
+        const _countdown = document.getElementById('container-countdown');
+		var questions = <?php echo json_encode($questions) ?>;
+		var reponses = <?php echo json_encode($rep) ?>;
+		var quizz = [];
 
-        function AfficherQuizz() {
-            for (var i = 0; i < quizz[0].length; i++) {
-                var question = quizz[0][i];
-                var reponse = quizz[1][i];
-                var questionString = JSON.stringify(question);
-                var reponseString = JSON.stringify(reponse);
-                console.log("Question " + (i+1) + ": " + questionString);
-                console.log("Réponse " + (i+1) + ": " + reponseString);
-                
-                console.log(question);
-                _quest.innerHTML = question;
+		for (let i = 0; i < questions.length; i++) {
+			let quiz = {
+				question: questions[i].question,
+				options: [
+					reponses[i].reponse1,
+					reponses[i].reponse2,
+					reponses[i].reponse3,
+					reponses[i].Bonne_rep
+				],
+				correct: reponses[i].Bonne_rep
+			};
+
+			quizz.push(quiz);
+		}
+
+        var numberquestion = 1;
+		var numquestion = 0;
+		var score = 0;
+		var isanswered = false;
+
+        _nbquestion.innerHTML = "Question " + numberquestion + "/10";
+		_quest.innerHTML = "<h1>" + quizz[numquestion].question + "</h1>";
+        _score.innerHTML = "Votre score : " + score + "/10";
+        
+
+		for (let i = 0; i < quizz[numquestion].options.length; i++) {
+
+            _answers.innerHTML = "<label class='answer'>" + "<input type='radio' name='option' value='option1'>" + quizz[numquestion].options[0] + "</label>" + "<label class='answer'>" + "<input type='radio' name='option' value='option3'>" + quizz[numquestion].options[1] + "</label>" + "<label class='answer'>" + "<input type='radio' name='option' value='option3'>" + quizz[numquestion].options[2] + "</label>" + "<label class='answer'>" + "<input type='radio' name='option' value='option3'>" + quizz[numquestion].options[3] + "</label>"
+
+		}
+
+        
+        displayQuestion(numquestion);
+
+        _submit.addEventListener('click', () => {
+            let selectedOption = document.querySelector('input[type=radio]:checked');
+            if (!selectedOption) {
+            alert('Veuillez sélectionner une réponse.');
+            return;
             }
+
+        let answer = selectedOption.value;
+        if (answer == quizz[numquestion].correct) {
+
+            score++;
+            _score.innerHTML = "Votre score : " + score + "/10";
         }
 
-        AfficherQuizz();
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="script.js"></script>
+         selectedOption.checked = false;
+
+        numquestion++;
+        numberquestion++;
+
+        if (numquestion < quizz.length) {
+        displayQuestion(numquestion);
+        } else {
+        _quest.innerHTML = "<h1>Vous avez terminé le quizz !</h1>";
+        _answers.innerHTML = "<h2>Votre score est de " + score + " sur " + quizz.length + "</h2>";
+        _submit.style.display = 'none';
+        _nbquestion.style.display = 'none';
+        _countdown.style.display = 'none';
+        _score.style.display = 'none';
+
+        }
+        });
+
+        function displayQuestion(num) {
+        _nbquestion.innerHTML = 'Question ' + numberquestion + '/' + quizz.length;
+        _quest.innerHTML = '<h1>' + quizz[num].question + '</h1>';
+
+        let optionsHtml = '';
+        for (let i = 0; i < quizz[num].options.length; i++) {
+            optionsHtml += '<label class="answer">';
+            optionsHtml += '<input type="radio" name="option" value="' + quizz[num].options[i] + '">';
+            optionsHtml += quizz[num].options[i];
+            optionsHtml += '</label>';
+        }
+
+        _answers.innerHTML = optionsHtml;
+        }
+
+        // _submit.addEventListener('click', function() {
+        // if (isanswered === false) {
+        //     var options = document.getElementsByName('option');
+        //     var selectedOption = '';
+        //     for (let i = 0; i < options.length; i++) {
+        //         if (options[i].checked) {
+        //             selectedOption = options[i].value;
+        //             break;
+        //         }
+        //     }
+        //     if (selectedOption === '') {
+        //         alert('Veuillez sélectionner une réponse.');
+        //         return;
+        //     }
+        //     isanswered = true;
+        //     if (selectedOption === quizz[numquestion].correct) {
+        //         score++;
+        //     }
+        //     numquestion++;
+        //     numberquestion++;
+        //     if (numquestion < quizz.length) {
+        //         _nbquestion.innerHTML = "Question " + numberquestion + "/10";
+        //         _quest.innerHTML = "<h1>" + quizz[numquestion].question + "</h1>";
+        //         _answers.innerHTML = '';
+        //         for (let i = 0; i < quizz[numquestion].options.length; i++) {
+        //             _answers.innerHTML += "<label class='answer'>" + "<input type='radio' name='option' value='" + quizz[numquestion].options[i] + "'>" + quizz[numquestion].options[i] + "</label>";
+        //         }
+        //     } else {
+        //         _nbquestion.innerHTML = '';
+        //         _quest.innerHTML = "<h1>Le quizz est terminé !</h1>";
+        //         _answers.innerHTML = "<p>Votre score est de " + score + " sur " + quizz.length + ".</p>";
+        //         _submit.style.display = 'none';
+        //     }
+        //     }
+        //     });
+</script>
+
+
+	</script>
 </body>
 </html>
-    
+
+
+
+
